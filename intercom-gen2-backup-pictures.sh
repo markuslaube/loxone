7e2ff94792d39a05896b97e58b8ab0916a523c0b
Markus Laube <markus.laube@noris.de>
2022-01-18 20:07:55 +0100

    make it cron-usable

--- intercom-gen2-backup-pictures.sh
+++ intercom-gen2-backup-pictures.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Das ist der zweite (erfolgreicher) Proof of Concept um Bilder von einer Loxone intercom2 in anderen Systemen weiter zu verarbeiten.
 # Ich verwende das Tool dazu, um ab und zu mal die Bilder von der Intercom wegzusichern
-# Achtung wscat ist offensichtlich nicht geegnet im Hintergrund zu laufen, das Script ist daher nicht CRON-fähig
+# Achtung wscat ist offensichtlich nicht geegnet im Hintergrund zu laufen, das Script ist daher nur CRON-fähig wenn man in pseudotty ein geeignetes Interface - wo nix rein kommt aber in der Theorie könnt wenn man wollte - angbibt
 #
 # Autor Markus Laube, markus@laube.email
-# Version: 2022-01-18 21:22
+# Version: 2022-01-18 22:00
 # Licence: GNU GENERAL PUBLIC LICENSE 3 - https://github.com/markuslaube/loxone/blob/main/LICENSE
 #
 intercom="172.16.2.190:80"                                       # Die IP und der Port der InterCom Gen.2
 wscatwait=20                                                     # Geschätzte Zeit für den Lauf der Funktion make_pictures -> brauchen wir bei -w vom wscat
 makewait=2                                                       # Geschätzte Zeit bis die Info von der Intercom im tempfile sind -> brauchen wir beim sleep in der funktion vom wscat
 picturedir="/tmp/intercom-bildertest"                            # -> Backup-Destination-Pfad
+pseudotty="< /dev/tty6"                                          # bei mir funktioniert /dev/tty6 als Input daher gibt es hier die Möglichkeit eine INPUTOption für wscat anzugeben
+                                                                 # ohne die Angabe wird Standard-Input verwendet, dann funktioniert das Script aber nicht im Cron
 tempfile="$(mktemp)"                                             # TempFile für Output aus WebSocket
 
 
@@ -36,10 +38,14 @@ function make_pictures () {
 make_pictures &
 
 # und jetzt die wscat Verbindung starten, -w  sorgt dafür das wir x Sekunden nach dem Command-Versand warten, das sollte angepasst werden, bei mir reicht das akutell
-wscat -c ${intercom} -s webrtc-signaling --slash -P -w ${wscatwait} -x '{"jsonrpc":"2.0","method":"getLastActivities","id":1,"params":[0,100]}' > ${tempfile}
+# wscat -c ${intercom} -s webrtc-signaling --slash -P -w ${wscatwait} -x '{"jsonrpc":"2.0","method":"getLastActivities","id":1,"params":[0,100]}' > ${tempfile}
+
+# ich bekomme auf meine Pi das Script in den Hintergrund in dem ich die Eingabe Umleite auf eine "normale" Console Allerdings ändert das nix an der Problematik mit den Waits, es löst nur das Cron-Thema
+wscat -c ${intercom} -s webrtc-signaling --slash -P -w ${wscatwait} -x '{"jsonrpc":"2.0","method":"getLastActivities","id":1,"params":[0,100]}' ${pseudotty} > ${tempfile}
 
 # Die Verbindung mit wscat ist jetzt eh weg, daher löschen wir jetzt auch das tempfile
 rm ${tempfile}
 
 # Nochmal Uhrzeit -> die Differenz in Sekunden kann man dann vom in der Variable wait runter nehmen, bitte Puffer lassen
 echo "DEBUG: Script beendet: $(date)"
+~
